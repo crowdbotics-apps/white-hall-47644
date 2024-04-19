753bfb81ffb719aa7ab9c00b7fe71956487b9617
Crowdbotics <team@crowdbotics.com>
2024-04-19 18:12:07 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "white-hall-47644",
+  url: "https://white-hall-47644.botics.co",
+  api: "https://white-hall-47644.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
